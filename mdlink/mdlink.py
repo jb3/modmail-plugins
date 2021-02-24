e94c3609c9b1ae3fddfa1b9c419330a29ad4f46c
Joe Banks <joseph@josephbanks.me>
2021-02-24 17:01:55 +0000

    Create mdlink.py

---
+++ mdlink/mdlink.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel
+
+class MDLink(commands.Cog):
+    def __init__(self, bot):
+        self.bot = botw
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.MODERATOR)
+    @checks.thread_only()
+    async def mdlink(self, ctx):
+        """Return a link to the modmail thread in markdown syntax."""
+        link = await self.bot.api.get_log_link(ctx.channel.id)
+        await ctx.send(f"`[Modmail Thread]({link})`")
+
+def setup(bot):
+    bot.add_cog(MDLink(bot))
